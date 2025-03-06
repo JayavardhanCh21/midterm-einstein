7adabb490b650d799c0c6f3cfc4b457542ecc0d8
Siva Naga jayavardhan Chintam <schin39@unh.newhaven.edu>
2025-03-05 22:44:54 -0500

    final commit

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuIcon = document.querySelector(".hamburger-icon");
-  const menuItems = document.querySelector(".menu-items");
-
-  menuIcon.addEventListener("click", function () {
-    menuItems.classList.toggle("show");
-  });
+document.getElementById("hamburger").addEventListener("click", function () {
+  document.querySelector(".header-nav").classList.toggle("active");
 });
-
-function updateTextBasedOnScreenSize() {
-  const paragraph = document.querySelector(".anthology1");
-
-  if (window.innerWidth <= 500) {
-    paragraph.innerHTML = "Hardcoded text for smaller screens.";
-  } else {
-    paragraph.innerHTML =
-      "The annus mirabilis papers are four papers that Albert Einstein published in the journal Annalen der Physik in 1905.";
-  }
-}
-
-updateTextBasedOnScreenSize();
-
-window.addEventListener("resize", updateTextBasedOnScreenSize);
